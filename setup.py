073cdba7eece28d30c4a681654dcec2ca6a2adec
aitgon <aitgon@users.noreply.github.com>
2023-04-27 07:06:48 +0200

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='Neural_Network_DNA_Demo',
+    version='0.1.0',
+    url='https://tagc.univ-amu.fr/en/users/gonzalez-aitor',
+    author='Aitor González',
+    author_email='aitor.gonzalez@univ-amu.fr',
+    description='Description of my package',
+    packages=find_packages(),
+    install_requires=[],
+)
